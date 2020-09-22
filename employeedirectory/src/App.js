226389e6506441315b04560558f2373ac240a272
Ken Cheng <kenfcheng@gmail.com>
2020-09-22 12:12:43 -0400

    styling in navBar.css

--- employeedirectory/src/App.js
+++ employeedirectory/src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Search from "./Components/Search.js";
+import Search from "./Search.js";
+import NavBar from "./NavBar.js";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <NavBar />
+      <div>
+        <Search />
+      </div>
     </div>
   );
 }
--- employeedirectory/src/Components/NavBar.js
+++ employeedirectory/src/Components/NavBar.js
@@ -3,7 +3,15 @@ import NavBar from "./NavBar.css";
 import Search from "./Search.js";
 
 const NavBar = () => {
-  return <div></div>;
+  return (
+    <div className="navBar">
+      <div className="logo bold">Employee Directory</div>
+      <div className="description">
+        <div className="searchEmployee">Search Employee</div>
+        <Search />
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
