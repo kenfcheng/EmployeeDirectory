310871c7615411a7d40a613cd6967fccf634c60c
Ken Cheng <kenfcheng@gmail.com>
2020-09-22 11:45:48 -0400

    imported Search to App.js and cleaned up Function App()

--- employeedirectory/src/Components/Search.js
+++ employeedirectory/src/Components/Search.js
@@ -3,20 +3,22 @@ import { Context } from "./Components/Context.js";
 
 // Search input
 Search = () => {
-    const [search, setSearch] = useState('')
-    const {employees, employeesDisplayed} = useContext(Context)
+  const [search, setSearch] = useState("");
+  const { employees, employeesDisplayed } = useContext(Context);
 
-    searchUpdate = ({target}) => {
-        const inputTerm = target.value
-        setSearch(inputTerm)
+  searchUpdate = ({ target }) => {
+    const inputTerm = target.value;
+    setSearch(inputTerm);
+    // This will live filter text as employee name is entered.
+    const result = employees.filter(function (employee) {
+      return employee.name.first.toLowerCase().indexOf(search.toLowerCase);
+    });
 
-        const result = employees.filter( function (employee) {
-            return employee.name.first.toLowerCase().indexOf(search)
-        }
+    employeesDisplayed([...filterResult]);
+  };
 
-    }
-}
-
-
+  return;
+  <input type="text" onChange={updateSearch} value={search}></input>;
+};
 
 export default Search;
