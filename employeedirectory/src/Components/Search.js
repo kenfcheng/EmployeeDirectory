06f14876a0b8f147a8a26f774fb402dffc25decf
Ken Cheng <kenfcheng@gmail.com>
2020-09-22 16:50:29 -0400

    Department and Position filters added in Search.js

--- employeedirectory/src/Components/Search.js
+++ employeedirectory/src/Components/Search.js
@@ -15,8 +15,25 @@ Search = () => {
     const result = employees.filter(function (employee) {
       return employee.name.first.toLowerCase().indexOf(search.toLowerCase);
     });
-
     employeesDisplayed([...filterResult]);
+  };
+
+  searchUpdate = ({ target }) => {
+    const inputTerm = target.value;
+    setSearch(inputTerm);
+    const result = position.filter(function (position) {
+      return position.name.toLowerCase().indexOf(search.toLowerCase);
+    });
+    positionDisplayed([...filterResult]);
+  };
+
+  searchUpdate = ({ target }) => {
+    const inputTerm = target.value;
+    setSearch(inputTerm);
+    const result = position.filter(function (department) {
+      return department.name.toLowerCase().indexOf(search.toLowerCase);
+    });
+    deptDisplayed([...filterResult]);
   };
 
   return;
