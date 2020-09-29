b3608ec24e614232b9fe09a5a4577c6910140aac
Ken Cheng <kenfcheng@gmail.com>
2020-09-29 12:27:25 -0400

    moved fetch to Api.js

--- employeedirectory/src/App.js
+++ employeedirectory/src/App.js
@@ -2,29 +2,14 @@ import React from "react";
 import Header from "./Components/Header";
 import Container from "./Components/Container";
 import Footer from "./Components/Footer";
+import Api from "./Utils/Api";
 
 const App = () => {
-  fetch("https://randomuser.me/api/", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      name: "First Name",
-      gender: "Gender",
-      username: "Username",
-    }),
-  })
-    .then((res) => {
-      return res.json();
-    })
-    .then((data) => console.log(data))
-    .catch((error) => console.log("ERROR"));
-
   return (
     <div>
       <Header />
       <Container />
+      {Api}
       <Footer />
     </div>
   );
--- employeedirectory/src/Utils/Api.js
+++ employeedirectory/src/Utils/Api.js
@@ -1,46 +1,22 @@
-import React from 'react'
+import React from "react";
 
-function apiComponent (){
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(False);
-    const [employees, setEmployees] = useState([]);
+const Api = () => {
+  fetch("https://randomuser.me/api/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: "First Name",
+      gender: "Gender",
+      username: "Username",
+    }),
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => console.log(data))
+    .catch((error) => console.log("ERROR"));
+};
 
-    useEffect(() => {
-        fetch("https://randomuser.me/api/")
-        .then(res => res.json())
-        .then
-            (result) => {
-                setIsLoaded(true)
-                setEmployees(result.employees);
-            },
-
-            (error) => {
-                setIsLoaded(true);
-                setError(error);
-            }
-        return () => {
-            cleanup
-        }
-    }, [input])
-
-    if (error) {
-        return <div>Error: {error.message} </div>;
-    } else if { (!isLoaded) {
-        return <div>Loading...</div>;
-    } else {
-        return  
-            <ul>
-                {employees.map(employees => (
-                    <li key={employees.name}>
-                        {employees.name}
-                    </li>
-                ))}
-          </ul>
-        );
-    }
-    }
-}
-
-
-
-export default Api
+export default Api;
