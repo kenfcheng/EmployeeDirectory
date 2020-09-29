c20db99aa21ebe29ca5b3e0a7fbecf0fec288c23
Ken Cheng <kenfcheng@gmail.com>
2020-09-29 12:15:15 -0400

    fetch for API

--- employeedirectory/src/App.js
+++ employeedirectory/src/App.js
@@ -2,7 +2,25 @@ import React from "react";
 import Header from "./Components/Header";
 import Container from "./Components/Container";
 import Footer from "./Components/Footer";
+
 const App = () => {
+  fetch("https://randomuser.me/api/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: "First Name",
+      gender: "Gender",
+      username: "Username",
+    }),
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => console.log(data))
+    .catch((error) => console.log("ERROR"));
+
   return (
     <div>
       <Header />
